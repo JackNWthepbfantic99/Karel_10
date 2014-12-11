67ada3f567784b3c9385a3697057546b25ba1806
JackNWthepbfantic99 <staceynavin@Jacks-MacBook-Air-2.local>
2014-12-11 12:17:59 +0000

    now found way out of maze two and three

--- Lab10.java
+++ Lab10.java
@@ -2,7 +2,7 @@
  * An algorithm helps an Athlete solve a maze, perhaps by keeping one hand against a wall.
  * 
  * @author Jack Navin-Weinstein
- * @version Thurs 4 December
+ * @version Thurs 11 December
  *
  */
 
@@ -16,34 +16,42 @@ public class Lab10 {
 		 String map = JOptionPane.showInputDialog("Which map?");
 		 Display.openWorld("maps/"+map+".map");
 		 Display.setSize(10, 10);
-		 Athlete athena = new Athlete(1, 1, Display.NORTH, Display.INFINITY);
-		 clearMaze(athena);
-	 }
-	 
-	 public static void clearMaze(Athlete arg) {
-		 
-	   while (!arg.nextToABeeper()){
-         while (!arg.frontIsClear()&&!arg.leftIsClear()&&!arg.rightIsClear()){
-         arg.turnAround();
-         arg.move();
-         }
-         while (arg.frontIsClear()&&!arg.leftIsClear()){
-         arg.move();
-         }
-         while (arg.frontIsClear()&&arg.leftIsClear()&&!arg.rightIsClear()){
-         arg.turnLeft();
-         arg.move();
-         }
-         while (!arg.frontIsClear()&&!arg.leftIsClear()&&arg.rightIsClear()){
-         arg.turnRight();
-         arg.move();
-         }
-         while (!arg.frontIsClear()&&arg.leftIsClear()){
-         arg.turnLeft();
-         arg.move();       
-         }
-	   }
+		 Athlete athena = new Athlete();
+ 		 clearMaze(athena);
+	    
     }
+	 
+	   
+		 
+    public static void clearMaze(Athlete arg){
+     while (!arg.nextToABeeper()){
+      
+       if (arg.frontIsClear() && !arg.leftIsClear()){
+       arg.move();
+       }
+       
+       if (arg.leftIsClear()){
+       arg.turnLeft();
+       arg.move();
+       }
+       
+       if (!arg.frontIsClear()&&!arg.leftIsClear()&&arg.rightIsClear()){
+       arg.turnRight();
+       arg.move();
+       }
+       
+       if (!arg.frontIsClear()&&!arg.leftIsClear()&&!arg.rightIsClear()){
+       arg.turnAround();
+       arg.move();
+       }
+     }
+    }
+} 
+
+      
+                
+         
+	   
+      
 		 //TODO Write an algorithm that directs arg to the beeper at the end of the maze
 	 
-}
